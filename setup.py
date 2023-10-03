4a975e0032378eadb0ad2b4655f80481343415f1
MR PEAKY <121819973+Peaky-XD@users.noreply.github.com>
2023-10-03 16:46:33 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
-#hello
+from setuptools import setup, find_packages
+
+setup(
+    name="apx",
+    version="0.1",
+    description="Python library for sending HTTP GET requests (Cython-compiled)",
+    author="Your Name",
+    author_email="x_spoilt@yahoo.com",  
+    url="https://github.com/Peaky-XD/apx",
+    packages=find_packages(),
+    install_requires=[
+        "certifi"
+        "_socket",
+        "ssl",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
